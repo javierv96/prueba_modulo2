ec8882dc276ca4d6771fd9aae375645214543d48
Javier Vergara <ja.vergarac96@gmail.com>
2024-01-31 01:56:59 -0300

    Identacion y Documentacion de codigo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,49 +1,49 @@
+//Tooltip
 var tooltipTriggerList = document.querySelectorAll(
-	'[data-bs-toggle="tooltip"]'
+  '[data-bs-toggle="tooltip"]'
 );
 var tooltipList = [...tooltipTriggerList].map(
-	(tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+  (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
 );
-
+//Funcion Enviar Correo
 $("#buttonEnviar").click(function (e) {
-	e.preventDefault();
-	alert("El correo fue enviado correctamente...");
+  e.preventDefault();
+  alert("El correo fue enviado correctamente...");
 });
-
-function scrollToTop() {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    });
-}
-
+//Funcion desaparecer contenido de tarjetas
 $(".tarjetaImagen").click(function () {
-	$(".contenido").toggle();
-	$(".contenido").show(1000);
+  $(".contenido").toggle();
+  $(".contenido").show(1000);
 });
-
-$("#tituloAboutUs").dblclick(function(){
-    $(this).css("color", "yellow");
+//Funcion doble click titulo AboutUs
+$("#tituloAboutUs").dblclick(function () {
+  $(this).css("color", "yellow");
 });
-
-$("#tituloDestacados").dblclick(function(){
-    $(this).css("color", "yellow");
+//Funcion doble click titulo Destacados
+$("#tituloDestacados").dblclick(function () {
+  $(this).css("color", "yellow");
 });
-
-$("#tituloContacto").dblclick(function(){
-    $(this).css("color", "yellow");
+//Funcion doble click titulo Contacto
+$("#tituloContacto").dblclick(function () {
+  $(this).css("color", "yellow");
 });
-
-$("#viajesChile").dblclick(function(){
-    $(this).css("color", "yellow");
+//Funcion doble click Viajes Chile (Footer)
+$("#viajesChile").dblclick(function () {
+  $(this).css("color", "yellow");
 });
-
+// Funcion ScrollTop
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+}
 // Muestra u oculta el botón según el desplazamiento de la página
 window.onscroll = function () {
-    var btn = document.getElementById("scrollToTopBtn");
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        btn.style.display = "block";
-    } else {
-        btn.style.display = "none";
-    }
+  var btn = document.getElementById("scrollToTopBtn");
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    btn.style.display = "block";
+  } else {
+    btn.style.display = "none";
+  }
 };
